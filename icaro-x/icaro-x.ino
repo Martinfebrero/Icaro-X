62f39b14f0d560cd85280443f07e0c43b2b0026f
JoaquinEduardoArreguez <arreguez.joaquin@gmail.com>
2016-08-11 18:48:14 -0300

    Rectificacion de media onda

--- icaro-x/icaro-x.ino
+++ icaro-x/icaro-x.ino
@@ -4,13 +4,17 @@
 
 // All definitions
 #define NUMCHANNELS 1
-#define SAMPFREQ 256                      // ADC sampling rate 256
-#define TIMER2VAL (1024/(SAMPFREQ))       // Set 256Hz sampling frequency                    
+#define SAMPFREQ 1000//256                      // ADC sampling rate 256
+#define TIMER2VAL (1024/(SAMPFREQ))       // Set 256Hz sampling frequency
+#define NSAMPLES  2                     // numero de samples a tomar antes de imprimir              
 
 // Global constants and variables
-volatile unsigned char CurrentCh;                       //Current channel being sampled.
-volatile unsigned int ADC_Value = 0;                	  //ADC current value
-volatile unsigned int ADC_Value_calibres[NUMCHANNELS];  // Valor cero de calibrado.
+volatile unsigned char currentCh;                       //Current channel being sampled.
+volatile unsigned int adc_value = 0;                	  //ADC current value
+volatile unsigned int adc_value_anterior = 0;
+volatile unsigned int adc_value_calibres[NUMCHANNELS];  // Valor cero de calibrado.
+
+volatile unsigned int cont = 0;
 
 //~~~~~~~~~~
 // Functions
@@ -18,22 +22,31 @@ volatile unsigned int ADC_Value_calibres[NUMCHANNELS];  // Valor cero de calibra
 
 void init_hardware_and_variables(){
   for (int i=0;i<NUMCHANNELS;i++){
-    ADC_Value_calibres[i] = 0;         // Inicio el calibre de todos los canales en 0, arbitrario.
+    adc_value_calibres[i] = 300;         // Inicio el calibre de todos los canales en 0, arbitrario.
   }
 }
 
 void setup() {
 
-  delay(5000);    //Delay en el inicio, por si el shield necesita "acomodarse".
+  delay(500);    //Delay en el inicio, por si el shield necesita "acomodarse".
 
  init_hardware_and_variables(); // Inicio del hardware.
  calibrar_canales();
 
  
  noInterrupts();
+
+ Serial.begin(9600);//57600
+
+ // Muestro el calibre para ver la línea.
+ for (int i = 0; i<500; i++){
+  Serial.println(adc_value_calibres[0]);
+ }
+
+
+
  FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
  FlexiTimer2::start();
- Serial.begin(57600);
  interrupts();          // Enable all interrupts after initialization has been completed
  }
 
@@ -41,9 +54,18 @@ void setup() {
 void Timer2_Overflow_ISR()
 {
   //Read the 6 ADC inputs and store current values in Packet
-  for(CurrentCh=0;CurrentCh<NUMCHANNELS;CurrentCh++){
-    ADC_Value = analogRead(CurrentCh);
-    Serial.println(ADC_Value);  //AÑADIDO
+  for(currentCh=0;currentCh<NUMCHANNELS;currentCh++){
+    adc_value = analogRead(currentCh);
+    if (adc_value < adc_value_calibres[currentCh]){
+      adc_value = adc_value_calibres[currentCh] + (adc_value_calibres[currentCh] - adc_value);
+    }
+
+    adc_value = (adc_value+adc_value_anterior)/2;
+
+    
+     if (cont >= NSAMPLES) {Serial.println((adc_value+adc_value_anterior)/2);cont = 0;}  //AÑADIDO
+     cont ++;
+    adc_value_anterior = adc_value;
   }
 }
 
@@ -63,9 +85,9 @@ void calibrar_canales(){
 }
 
 void Timer2_Overflow_Calibre_ISR(){
-  for(CurrentCh=0;CurrentCh<NUMCHANNELS;CurrentCh++){
-    ADC_Value = analogRead(CurrentCh);
-    Serial.println(ADC_Value_calibres[CurrentCh]);
-    ADC_Value_calibres[CurrentCh] = ( ADC_Value_calibres[CurrentCh] + ADC_Value ) / 2; 
+  for(currentCh=0;currentCh<NUMCHANNELS;currentCh++){
+    adc_value = analogRead(currentCh);
+    Serial.println(adc_value_calibres[currentCh]);
+    adc_value_calibres[currentCh] = ( adc_value_calibres[currentCh] + adc_value ) / 2; 
   }
 }
